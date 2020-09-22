085a5aa2d5eceee7744d4bceeb926ae95d30635d
oguzVstudio <oguzVstudio@users.noreply.github.com>
2020-09-22 09:35:58 +0300

    .

--- sTest.Api/Program.cs
+++ sTest.Api/Program.cs
@@ -2,15 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using sTest.MetaData.DataAccess.Data;
 
 namespace sTest.Api
 {
     public class Program
     {
+
+#if DEBUG
+
+        #region manual db initialize, manual migrations
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -22,5 +30,33 @@ namespace sTest.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        #endregion
+
+
+
+#else
+        #region auto initialize db and add migrations
+    public async static Task Main(string[] args)
+        {
+            var webHost = BuildWebHost(args).Build();
+
+            using (var serviceScope = webHost.Services.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                await dbContext.Database.MigrateAsync();
+            }
+
+            await webHost.RunAsync();
+        }
+
+        public static IWebHostBuilder BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+
+            .UseStartup<Startup>();
+        #endregion
+
+#endif
     }
 }
+
+    
